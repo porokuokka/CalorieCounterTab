d32bfa47ae9df9a7b13da7109d0ba89d9a7958b0
Iida Porokuokka <iida.porokuokka@jyu.fi>
2013-06-24 23:13:13 +0300

    First commit of tablet app, very much work todo!

---
+++ CalorieCounterTab/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using CalorieCounterTab;
+using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CalorieCounterTab
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private List<Item> items;
+        //private List<Item> search;
+        private IMobileServiceTable<Item> itemTable = App.MobileService.GetTable<Item>();
+        MessageBox ms = new MessageBox();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.LayoutRoot.Children.Add(ms);
+            LoadData();
+        }
+
+        public void ShowError(string error)
+        {
+            ms.showMessage(error);
+            
+        }
+
+        /// <summary>
+        /// Loads items from mobile service to itemslist
+        /// </summary>
+        public async void LoadData()
+        {
+            try
+            {
+                items = await itemTable.ToListAsync();
+            }
+            catch (HttpRequestException)
+            {
+                var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+                string s = loader.GetString("HttpError");
+                ShowError(s);
+            }
+            //testing
+            ListViewItems.ItemsSource = items;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Changes item when user clicks it on ItemDetails-listview
+        /// TODO:Test if cracks when played with
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ListViewItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ItemDetails.DataContext = ListViewItems.SelectedItem;
+            ItemDetails.Visibility = Visibility.Visible;
+            AddControl add = new AddControl(ListViewItems.SelectedItem as Item);
+            GridAdd.Children.Add(add);
+        }
+
+        private void TextBlock_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ms.showMessage("Tapped");
+        }
+
+    }
+}
---
+++ CalorieCounterTab/MessageBox.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CalorieCounterTab
+{
+    public sealed partial class MessageBox : UserControl
+    {
+        public MessageBox()
+        {
+            this.InitializeComponent();
+            this.Visibility = Visibility.Collapsed;
+        }
+
+        public void showMessage(string message)
+        {
+            this.Visibility = Visibility.Visible;
+            TextBlockMessage.Text = message;
+        }
+
+        private void ButtonOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+        }
+    }
+}
